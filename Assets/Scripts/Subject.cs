699dc21670ff0ef1e2312a801520104c57ff5da2
erdevelop <erdalustunol@gmail.com>
2023-02-18 18:11:10 +0300

    done

---
+++ Assets/Scripts/Subject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Subject : MonoBehaviour
+{
+    private List<Observer> _observers = null;
+
+    [SerializeField] private SubjectType _subjectType;
+    public SubjectType SubjectType => _subjectType;
+
+    private void Start()
+    {
+        ObserverManager.Instance.RegisterSubject(this);
+    }
+
+    public void RegisterObserver(Observer observer)
+    {
+        if (_observers == null)
+            _observers = new List<Observer>();
+
+        _observers.Add(observer);
+    }
+
+    public void Notify(NotificationType notificationType)
+    {
+        foreach (var observer in _observers)
+        {
+            observer.OnNotify(notificationType);
+        }
+    }
+}
